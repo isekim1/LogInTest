aaf8b335899ab9644de711d45c872416242f12ff
ise <taimaxxxxx@gmail.com>
2018-11-28 10:03:32 +0900

    #2 asdsdsadas

---
+++ Assets/04 Script/ChangeScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    //씬전환
+    //public void ChangeGameScene()
+    //{
+    //    SceneManager.LoadScene ("GameLogin");
+    //}
+
+        
+
+    //2개의 장면 씬전환   ->  이후에 Crtl Shift B 빌드에 각씬올리기 
+
+    public void NextButtonClick()
+    {
+        SceneManager.LoadScene ("GameLogin");
+    }
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Assets/04 Script/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public GameObject cu;
+   
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void CloseButtonClick()
+    {
+        cu.SetActive(false);
+    }
+}
